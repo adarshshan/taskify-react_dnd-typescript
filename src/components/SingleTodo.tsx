b8e0c6252f5c70f4dd68b6ba8b1938e28a6d2fc8
Adarsh <adarshshanu3@gmail.com>
2024-03-29 09:50:13 +0530

    the application is almost completed

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Todo } from '../model'
+import { CiEdit } from "react-icons/ci";
+import { MdDelete, MdDone } from "react-icons/md";
+
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
+  }
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  }
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>, id: number) => {
+    e.preventDefault();
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo)));
+    setEdit(false);
+  }
+  const inputRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+  return (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          className='todos__single--text'
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)} />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
+
+      <span className="icon" onClick={() => {
+        if (!edit && !todo.isDone) setEdit(!edit);
+      }}><CiEdit /></span>
+      <span className="icon" onClick={() => handleDelete(todo.id)}><MdDelete /></span>
+      <span className="icon" onClick={() => handleDone(todo.id)}><MdDone /></span>
+    </form>
+  )
+}
+
+export default SingleTodo
